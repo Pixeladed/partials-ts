31cefc7e3914e7c28ad15c1f731de956625033af
Minh <vietminhle98@gmail.com>
2021-05-03 13:54:26 +1000

    Correct PartialOptional type

--- partials.ts
+++ partials.ts
@@ -6,4 +6,4 @@ export type PartialRequired<T, K extends keyof T> = T & Pick<Required<T>, K>
 /**
  * Produces type T where property K is optional
  */
-export type PartialOptional<T, K extends keyof T> = T & Pick<Partial<T>, K>
+export type PartialOptional<T, K extends keyof T> = Partial<T> & Pick<T, Exclude<keyof T, K>>
